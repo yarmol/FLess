c5cd425344e05216397bc8726fea113fcc0cb98e
vitaly <vitaly@yarmolenko.sirius.gospodar.org>
2015-06-17 11:37:08 +0300

    class diagramm

--- src/SimpleCalc.java
+++ src/SimpleCalc.java
@@ -1,3 +1,4 @@
+import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -12,6 +13,7 @@ public class SimpleCalc  {
 	SimpleCalc(){
 		
 		JFrame mainFrame = new JFrame();
+		JPanel mainPanel 		= new JPanel();
 		JPanel buttonPanel 	 	= new JPanel();
 		JPanel monitorPanel 	 = new JPanel();
 		Model model 	 = Model.getModel();
@@ -20,14 +22,32 @@ public class SimpleCalc  {
 		GridLayout buttonGrid = new GridLayout(4,4);
 		buttonPanel.setLayout(buttonGrid);
 		
+			
+		//monitor element
+		JTextField monitor = new JTextField();
+		monitor.setSize(50, 200);
+		monitor.setColumns(25);
+		
+		monitorPanel.add(monitor);
+		
+		BorderLayout mainLayout = new BorderLayout();
+		mainLayout.addLayoutComponent(monitorPanel, BorderLayout.NORTH);
+		mainLayout.addLayoutComponent(buttonPanel, BorderLayout.CENTER);
+		mainPanel.setLayout(mainLayout);
+	
 		//defining buttons 
-		String simpleButtonOps = "1234567890./*-+";
+		String simpleButtonOps = "1234567890./*-+=";
 		ArrayList<String> s = new ArrayList<>();
-		String[] buttonOps;
-		Arrays.asList(simpleButtonOps.toCharArray()).forEach((ñ) -> buttonOps.add(c));	
+		String[] buttonOps = new String[simpleButtonOps.length()];
+		int i = 0;
+		for (char ch : simpleButtonOps.toCharArray()) {
+			buttonOps[i] = String.valueOf(ch);
+			++i;
+		}
+		
 	 	
 		CalcBuilder cb = new CalcBuilder(buttonOps,buttonPanel);
-		mainFrame.setContentPane(buttonPanel);
+		mainFrame.setContentPane(mainPanel);
 		mainFrame.setVisible(true);
 	
 	}
